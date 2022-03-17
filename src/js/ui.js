ed0bd70ee3137c16577cdd2f9ccc387960f7f235
dev <dev@golgoru.co.kr>
2022-03-18 08:30:19 +0900

    손해사매칭 작업중

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,6 +1,17 @@
+// GNB활성화를
+if (page == 'matching') {
+  $('.gnbList__item').eq(0).addClass('active')
+} else if (page == 'service') {
+  $('.gnbList__item').eq(1).addClass('active')
+} else if (page == 'event') {
+  $('.gnbList__item').eq(2).addClass('active')
+} else if (page == 'cs-center') {
+  $('.gnbList__item').eq(3).addClass('active')
+}
+
 // 헤더
 var headerEvt = function () {$()
-  var header = $('.header')
+  var header = $('.l-header')
   $(window).on('scroll', function () {
     var scTop = $(this).scrollTop()
     // console.log('scroll top', scTop)
@@ -38,11 +49,6 @@ function goTop() {
   })
 }
 
-// 숫자 천단위 콤마
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-}
-
 // 버튼 텍스트 애니효과
 $.fn.btnAni = function () {
   var thisBtn = document.querySelectorAll('.btn-step-chg')
@@ -54,7 +60,7 @@ $.fn.btnAni = function () {
 function openModal(modalName) {
   console.log('openModal')
   $('html').addClass('ovh')
-  $('.modal[data-modal="' + modalName + '"]').show()
+  $('.modal[data-modal="' + modalName + '"]').addClass('show')
   if (modalName == 'reception02') {
     $('.reception-form .form').css('overflow' , 'initial').css('overflow' , 'hidden')
   }
@@ -62,8 +68,10 @@ function openModal(modalName) {
 
 // 모달 닫기
 function closeModal(modal) {
-  $('.modal[data-modal=' + modal + ']').hide()
-  $('html').removeClass('ovh')
+  $('.modal[data-modal=' + modal + ']').removeClass('show')
+  if ($('.modal.show').length < 1) {
+    $('html').removeClass('ovh')
+  }
 }
 
 // 칩 토글
@@ -100,8 +108,43 @@ $.fn.textArea = function () {
 
 // formInp 
 $.fn.formInpTxt = function () {
-  
+  var inpBody = Array
+  return this.each(function (i) {
+    inpBody[i] = $(this)
+    var input = inpBody[i].children('.formInp__txt')
+    var btnDel = inpBody[i].children('.js-del')
+    input.on('keyup', function () {
+      var txtLeng = $(this).val().length
+      if (txtLeng > 0) {
+        $(this).next('.formInp__del').show()
+      } else {
+        $(this).next('.formInp__del').hide()
+      }
+    })
+  })
 }
+
+// 버튼 이벤트
+// $.fn.buttonInnerAni = function () {
+//   var curBtn = this
+//   curBtn.on('hover', function () {
+//     var parentOffset = $(this).offset(); 
+   
+//     var relX = e.pageX - parentOffset.left;
+//     var relY = e.pageY - parentOffset.top;
+//     $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+//     $(this).prev(".su_button_circle").removeClass("desplode-circle");
+//     $(this).prev(".su_button_circle").addClass("explode-circle");
+//   }, function () {
+//     var parentOffset = $(this).offset(); 
+
+//     var relX = e.pageX - parentOffset.left;
+//     var relY = e.pageY - parentOffset.top;
+//     $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
+//     $(this).prev(".su_button_circle").removeClass("explode-circle");
+//     $(this).prev(".su_button_circle").addClass("desplode-circle");
+//   })
+// }
 
 // 플러그인 불러오기
 function loadUi () {
@@ -109,6 +152,6 @@ function loadUi () {
   $('[data-chip="toggle"]').chipToggle()
   $('.c-scroll-bar').niceScroll();
   $('.textArea').textArea();
-  $('.formInp__wrap').formInpTxt();
+  $('.formInp').formInpTxt();
 }
 loadUi ()
