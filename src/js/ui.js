073710782279620492b7d1c9f7e036ea465d4ede
dev <dev@golgoru.co.kr>
2022-04-07 17:21:05 +0900

    병원찾기

--- src/js/ui.js
+++ src/js/ui.js
@@ -66,6 +66,10 @@ function openModal(modalName) {
   if (modalName == 'reception02') {
     $('.reception-form .form').css('overflow' , 'initial').css('overflow' , 'hidden')
   }
+  // 병원찾기 모달일 경우는 지도보기 동적 생성
+  if (modalName == 'hospital') {
+    createMap ('hospital-map', '.modal-hospital .hospital-addr', '.modal-hospital .hospital-name')
+  }
 }
 
 // 모달 닫기
@@ -267,6 +271,50 @@ $.fn.boardList = function () {
       $(this).parents('.boardList__row').removeClass('open')
     })
   })
+}
+
+// 카카오지도
+function createMap (mapEl, addr, infoWindow) {
+  // 지도 실행 스크립트
+  var mapContainer = document.getElementById(mapEl), // 지도를 표시할 div 
+  mapOption = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표 아무곳이든 필수입력
+    level: 4 // 지도의 확대 레벨
+  };  
+
+  // 도로명주소 - 데이터를 변수에 대입해도 됩니다.
+  // const address = some data
+  const address = $(addr).text()
+
+  // 손해사정사 이름 - 데이터를 변수에 대입해도 됩니다.
+  // const agentName = some data
+  const infoName = $(infoWindow).text()
+
+  // 지도를 생성합니다    
+  var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+  // 주소-좌표 변환 객체를 생성합니다
+  var geocoder = new kakao.maps.services.Geocoder();
+
+  // 주소로 좌표를 검색합니다
+  geocoder.addressSearch(address, function(result, status) {
+    // 정상적으로 검색이 완료됐으면 
+    if (status === kakao.maps.services.Status.OK) {
+      var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+      // 결과값으로 받은 위치를 마커로 표시합니다
+      var marker = new kakao.maps.Marker({
+          map: map,
+          position: coords
+      });
+      // 인포윈도우로 장소에 대한 설명을 표시합니다 (필요한 경우에만)
+      var infowindow = new kakao.maps.InfoWindow({
+          content: '<div class="info-window d-flex-c-c">' + infoName + ' </div>'
+      });
+      infowindow.open(map, marker);
+      // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+      map.setCenter(coords);
+    } 
+  });      
 }
 
 // 플러그인 불러오기
